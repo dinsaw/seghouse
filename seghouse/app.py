726f338b529c9ceef6f889a45974bbd901d3c70a
Dinesh Sawant <dineshsawant300@gmail.com>
2021-01-08 21:07:18 +0530

    Remove gz files if s3 dir is specified

--- seghouse/app.py
+++ seghouse/app.py
@@ -18,7 +18,10 @@ def app():
 @app.command()
 @click.option("--config-file", "-cf", type=click.Path(exists=True))
 @click.option("--s3-dir", "-s3d",
-              help="S3 Directory. We will look for *.gz files in this directory. Ensure that you have configured aws credentials using aws cli. Make sure that this directory contains files less than 100.")
+              help="S3 Directory. We will look for *.gz files in this directory. "
+                   "Ensure that you have configured aws "
+                   "credentials using aws cli. "
+                   "Make sure that this directory contains files less than 100.")
 @click.option("--source-dir", "-sd", type=click.Path(exists=True))
 @click.option("--namespace", "-ns", required=True, help="Will be used to create database/namespace in warehouse", )
 def send(config_file: str, s3_dir: str, source_dir: str, namespace: str):
@@ -26,10 +29,13 @@ def send(config_file: str, s3_dir: str, source_dir: str, namespace: str):
     logging.info(f"config_file={config_file}")
     app_conf = configuration.from_yaml(config_file)
 
-    if s3_dir:
-        source_dir = aws_wrapper.download_gz_files(s3_dir)
+    try:
+        if s3_dir:
+            source_dir = aws_wrapper.download_gz_files(s3_dir)
 
-    job = send_to_warehouse.SendToWarehouseJob(app_conf, source_dir, namespace)
-    job.execute()
-
-    shutil.rmtree(source_dir)
+        job = send_to_warehouse.SendToWarehouseJob(app_conf, source_dir, namespace)
+        job.execute()
+    finally:
+        if s3_dir:
+            logging.info(f"Removing directory {source_dir}")
+            shutil.rmtree(source_dir)
